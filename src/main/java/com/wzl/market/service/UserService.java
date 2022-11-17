df31a02f9344df95e0622a95923cc2eed6da8c26
BlackBear2003 <348358584@qq.com>
2022-11-17 21:32:06 +0800

    do gitignore

--- src/main/java/com/wzl/market/service/UserService.java
+++ src/main/java/com/wzl/market/service/UserService.java
@@ -19,5 +19,6 @@ public interface UserService {
     public ResponseResult getAllInfo(int id);
     public ResponseResult getPublicInfo(int id);
 
+    @Transactional
     ResponseResult getUsersByPage(int current, int size);
 }
